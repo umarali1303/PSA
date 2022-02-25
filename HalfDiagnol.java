75b5ace872cde09f4018a2c20e39773259c239e4
umarali1303 <78634200+umarali1303@users.noreply.github.com>
2022-02-25 22:58:56 +0530

    Create HalfDiagnol.java

---
+++ HalfDiagnol.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[][] arr=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++){
+            int k=0;
+            for(int j=i;j<n;j++){
+                System.out.println(arr[k][j]);
+                k++;
+            }
+        }
+        
+    }
+
+}
